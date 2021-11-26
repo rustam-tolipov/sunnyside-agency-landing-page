375240df2e13e96967fba6cff6789b5f5174d741
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-11-26 14:34:51 +0500

    Add Hamburger to mobile designe

---
+++ src/components/Hamburger.js
@@ -0,0 +1,40 @@
+// Create hamburger menu
+import React from 'react';
+import { useState } from 'react';
+import hamburger from '../images/icon-hamburger.svg';
+import './Hamburger.css';
+
+const Hamburger = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
+
+  return (
+    <div className="hamburger_container">
+      <button className="hamburger_button" onClick={handleClick} aria_label="Menu">
+        <img src={hamburger} alt="Menu" />
+      </button>
+      <div className={`hamburger_menu ${isOpen ? 'hamburger_menu--open' : ''}`}>
+        <ul className="hamburger_menu__list">
+          <li className="hamburger_menu__list-item">
+            <a href="/About">About</a>
+          </li>
+          <li className="hamburger_menu__list-item">
+            <a href="/Services">Services</a>
+          </li>
+          <li className="hamburger_menu__list-item">
+            <a href="/Projects">Projects</a>
+          </li>
+          <li className="hamburger_menu__list-item contact">
+            <a href="/Contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Hamburger;
