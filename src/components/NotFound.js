a00eb656202e565570abceedd9f539160082b375
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-11-26 15:30:57 +0500

    Add Video to NotFound page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './NotFound.css';
 import GoBack from './GoBack';
+import underConstruction from '../images/under-construction.mp4';
+// Add background video to the page
 
 const NotFound = () => (
   <div className="not-found">
+    <video autoPlay loop muted className="not-found__video-container">
+      <source src={underConstruction} type="video/mp4" />
+    </video>
     <GoBack />
     <h1 className="under__construction_title">This page is under construction</h1>
     <h2 className="under__construction_warning">Please come back later</h2>
