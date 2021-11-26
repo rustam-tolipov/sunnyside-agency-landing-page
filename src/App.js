d24b23c71e2c57612757a2fa83cacbcd1bb98357
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-11-26 14:30:19 +0500

    Install `index.js` and `App.js`

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Container from './components/Container';
+
+function App() {
+  return (
+    <div className="App">
+      <Container />
+    </div>
+  );
+}
+
+export default App;
